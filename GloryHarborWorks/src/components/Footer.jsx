ca11921dea025fef1cb0d0be4229c46685973dbd
cnajayi <cnajayi@loyola.edu>
2026-02-09 17:39:47 -0500

    Add basic navbar and footer components

---
+++ GloryHarborWorks/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white py-6 mt-10">
+      <div className="text-center">
+        <p className="text-sm">
+          © {new Date().getFullYear()} Glory Harbor Works. 
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
